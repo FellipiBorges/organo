a3ac678896ecf53dc2f2edd1c9a62cc3a39ebf29
FellipiBorges <fellipiborgesmc@gmail.com>
2024-01-03 15:33:31 -0300

    Firts Commit of the year

---
+++ src/componentes/banners/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <Header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal do Organo"/>
+        </Header>
+    )
+}
+
+export default Banner
